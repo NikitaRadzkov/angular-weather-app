4e170fb1c9e776c8b435c9bbe88e457a98dfc18b
Nikita Radzkou <nikita.radzkou@gmail.com>
2023-02-07 14:44:23 +0300

    Create simple angular weather app

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  weatherApiBaseUrl: 'https://bestweather.p.rapidapi.com/weather/',
+  XRapidAPIKeyName: 'X-RapidAPI-Key',
+  XRapidAPIKeyValue: '5bb6b6f563msh432706945a4e227p155b8fjsn10ee61d9736b',
+  XRapidAPIHostName: 'X-RapidAPI-Host',
+  XRapidAPIHostValue: 'bestweather.p.rapidapi.com'
+};
